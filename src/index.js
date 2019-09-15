da73037c98e4f8da9f52428ea72a7e3509fbdc59
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-09-15 08:21:03 -0500

    Added connect/disconnect actions

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ const io = socketio(server);
 
 app.use(express.static(publicDir));
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => {    
     socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left.');
     });
 });
 
