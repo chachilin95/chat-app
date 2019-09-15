d550a269f731c0d5487eb0e095b27f25d47e318a
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-09-15 09:27:43 -0500

    Created profanity filter

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,15 @@
+const ProfanityFilter = require('bad-words');
+
 const generateGoogleMapsLink = ({ latitude, longitude }) => (
     `https://google.com/maps?q=${latitude},${longitude}`
 );
 
+const profanityTest = (message) => {
+    const filter = new ProfanityFilter();
+    return filter.isProfane(message);
+};
+
 module.exports = {
-    generateGoogleMapsLink
+    generateGoogleMapsLink,
+    profanityTest
 };
