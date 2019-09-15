9b294de61d7d60aa91ce5d721da8ad4af215e346
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-09-15 08:16:05 -0500

    deleted app.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-const { app, server, port } = require('./app');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, '../public');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(publicDir));
+
+io.on('connection', (socket) => {
+    socket.emit('message', 'Welcome!');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
+});
 
 server.listen(port, () => {
     console.log('Server is up on port ' + port);
-})
+});
