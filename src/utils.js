cdc0c60186bf63cfe33b70feda7fa342341b991b
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-09-16 16:45:39 -0500

    created singleton instance of profanity filter

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
 const ProfanityFilter = require('bad-words');
+const filter = new ProfanityFilter();
 
 const generateGoogleMapsLink = ({ latitude, longitude }) => (
     `https://google.com/maps?q=${latitude},${longitude}`
 );
 
 const profanityTest = (message) => {
-    const filter = new ProfanityFilter();
     return filter.isProfane(message);
 };
 
